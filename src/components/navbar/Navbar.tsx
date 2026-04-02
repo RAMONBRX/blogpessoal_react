831b991ca32c93639564c86110f5f3477ef1a97f
RAMONBRX <ramon.all98@hotmail.com>
2026-04-02 11:10:58 -0300

    ✨ New Feature:  Construção das rotas dos componentes.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <>
@@ -5,8 +7,9 @@ function Navbar() {
       className="w-full flex justify-center py-4 bg-indigo-900 text-white">
         <div 
         className="container flex justify-between text-lg mx-8">
+           <Link to= "/home" className="text-2xl font-bold">
             Blog Pessoal
-
+          </Link>
             <nav 
             className="flex gap-4">
                 Postagens
